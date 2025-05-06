b14f2894d8c8fdf21c50bebe3b3ea1cfca85e8be
Alejandra Charcape <alealmu2002@hotmail.com>
2025-05-07 01:24:44 +0200

    Initial commit: Added Sumador library with Maven setup

---
+++ src/main/java/com/ejemplo/Sumador.java
@@ -0,0 +1,15 @@
+package com.ejemplo;
+
+public class Sumador {
+
+    /**
+     * Función que toma dos números enteros como entrada y devuelve su suma.
+     *
+     * @param num1 El primer número entero.
+     * @param num2 El segundo número entero.
+     * @return La suma de num1 y num2.
+     */
+    public static int sumar(int num1, int num2) {
+        return num1 + num2;
+    }
+}
